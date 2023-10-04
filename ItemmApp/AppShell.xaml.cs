f12cdd7526ed15b6f18d410f6efbbd79809cabc2
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-10-04 09:36:40 -0300

    sei n

--- ItemmApp/AppShell.xaml.cs
+++ ItemmApp/AppShell.xaml.cs
@@ -5,6 +5,7 @@ public partial class AppShell : Shell
 	public AppShell()
 	{
 		InitializeComponent();
+        AppShell.SetNavBarIsVisible(this, false);
 
         Routing.RegisterRoute(nameof(LoginPage), typeof(LoginPage));
         Routing.RegisterRoute(nameof(MainPage), typeof(MainPage));
