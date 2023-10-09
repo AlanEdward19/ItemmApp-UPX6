7f3a69100199927db101ec232edb72614c74df89
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-10-09 15:27:26 -0300

    mvvmit toolkit

--- ItemmApp/MauiProgram.cs
+++ ItemmApp/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using ItemmApp.ViewModel;
+﻿using CommunityToolkit.Maui;
+using ItemmApp.ViewModel;
 using Microsoft.Extensions.Logging;
 
 namespace ItemmApp;
@@ -10,7 +11,8 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
+            .UseMauiCommunityToolkit()
+            .ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
