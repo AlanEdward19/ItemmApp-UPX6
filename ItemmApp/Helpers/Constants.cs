80c69c1d161d5a220203ab4f46bc10085970d8b4
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2023-10-16 17:11:25 -0300

    helpers

---
+++ ItemmApp/Helpers/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace ItemmApp.Helpers
+{
+    public class Constants
+    {
+        public static string ApiUrl => DeviceInfo.Platform == DevicePlatform.WinUI ? "https://localhost:5000" : "http://10.0.2.2:5000";
+    }
+}
+
