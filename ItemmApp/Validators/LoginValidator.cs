7b0839c2ec3debaf0b4dd640c123a7cec35a6a8e
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2023-10-16 17:11:55 -0300

    validator

---
+++ ItemmApp/Validators/LoginValidator.cs
@@ -0,0 +1,15 @@
+﻿using Flunt.Validations;
+using ItemmApp.Models.Request;
+
+namespace ItemmApp.Validators;
+
+public class LoginValidator : Contract<LoginRequest>
+{
+    public LoginValidator(LoginRequest request)
+    {
+        Requires()
+            .IsNotNullOrEmpty(request.Email, "Email", "E-mail não pode ser vazio")
+            .IsEmail(request.Email, "Email", "E-mail inválido")
+            .IsNotNullOrWhiteSpace(request.Password, "Senha", "Senha não pode ser vazia");
+    }
+}
