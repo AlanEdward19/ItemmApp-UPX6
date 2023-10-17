29c38f95913374ba4be8323b16f27fb96818b7cc
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2023-10-16 22:32:13 -0300

    utils

--- ItemmApp/Helpers/Constants.cs
+++ ItemmApp/Helpers/Constants.cs
@@ -2,7 +2,7 @@
 {
     public class Constants
     {
-        public static string ApiUrl => DeviceInfo.Platform == DevicePlatform.WinUI ? "https://localhost:5000" : "http://10.0.2.2:5000";
+        public static string ApiUrl => DeviceInfo.Platform == DevicePlatform.WinUI ? "https://localhost:5000" : "192.168.184.109:5000";
     }
 }
 
---
+++ ItemmApp/Helpers/Converter.cs
@@ -0,0 +1,34 @@
+﻿using Xceed.Words.NET;
+
+namespace ItemmApp.Helpers
+{
+    public static class Converter
+    {
+        public static void FillDOCX(string docxTemplatePath, string docxOutputPath, Dictionary<string, string> changes)
+        {
+            using (var doc = DocX.Load(docxTemplatePath))
+            {
+                foreach (var change in changes)
+                {
+                    string searchText = change.Key;
+                    string newValue = change.Value;
+
+                    // Leitura do DOCX para encontrar a posição do texto
+                    foreach (var paragraph in doc.Paragraphs)
+                    {
+                        if (paragraph.Text.Contains(searchText, StringComparison.OrdinalIgnoreCase))
+                        {
+                            // Encontrou o texto no parágrafo, substitui com o novo valor
+                            paragraph.ReplaceText(searchText, newValue, false, System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+                        }
+                    }
+                }
+
+                // Salva o documento modificado
+                doc.SaveAs(docxOutputPath);
+            }
+        }
+
+        public static string FormatCpf(string cpf) => Convert.ToUInt64(cpf).ToString(@"000\.000\.000\-00");
+    }
+}
