3561e2cd3b6173c71e49b3b02b0bf5c22a695207
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2023-10-18 17:05:52 -0300

    azure url

--- ItemmApp/Helpers/Constants.cs
+++ ItemmApp/Helpers/Constants.cs
@@ -2,7 +2,7 @@
 {
     public class Constants
     {
-        public static string ApiUrl => DeviceInfo.Platform == DevicePlatform.WinUI ? "https://localhost:5000" : "192.168.184.109:5000";
+        public static string ApiUrl => "https://ittem-api-apim.azure-api.net";
     }
 }
 
